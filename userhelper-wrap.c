0ea3f76e5aca6e66c37d4f6f666c197086493ae7
otto <none@none>
1997-10-13 11:30:16 -0500

    On the road to converting userpasswd to the new wrapper functions around userhelper.  Now gotten to the point where it works as well as it did without them.  Lots of stuff to be done to the wrapper functions though... gdk_input and sigchld handler and such.

--- userhelper-wrap.c
+++ userhelper-wrap.c
@@ -1,7 +1,202 @@
-/* -*-Mode: c-*-
- * Author:     Otto Hammersmith
- * File:       .c
- * Created:    <>
- * Time-stamp: <97/04/10 22:23:18 ohammers>
- * Purpose:
+/* -*-Mode: c-*- */
+/* Copyright (C) 1997 Red Hat Software, Inc.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+
+#include "userhelper-wrap.h"
+
+#define MAXLINE 512
+
+int childout[2];
+int childin[2];
+
+/* the only difference between this and userhelper_run_chfn() is the
+ * final exec() call... I want to avoid all duplication, but I can't
+ * think of a clean way to do it right now...
+ */
+void
+userhelper_run_passwd()
+{
+  GtkWidget* message_box;
+  char* prompt;
+  int prompt_type;
+
+  pid_t pid;
+  int childstatus;
+  char outline[MAXLINE];
+  int count;
+
+  fd_set rfds;
+  fd_set wfds;
+  struct timeval tv;
+  int retval;
+
+  signal(SIGCHLD, userhelper_sigchld);
+
+  if(pipe(childout) < 0 || pipe(childin) < 0)
+    {
+      fprintf(stderr, "Pipe error.\n");
+      exit(1);
+    }
+
+  if((pid = fork()) < 0)
+    {
+      fprintf(stderr, "Cannot fork().\n");
+    }
+  else if(pid > 0)		/* parent */
+    {
+      close(childout[1]);
+      close(childin[0]);
+
+      FD_ZERO(&rfds);
+      FD_SET(childout[0], &rfds);
+      FD_ZERO(&wfds);
+      FD_SET(childin[1], &wfds);
+
+      /* switch to gdk_input() */
+      retval = select(childout[0] + 1, &rfds, &wfds, NULL, NULL);
+      while(retval > 0)
+      {
+	if(FD_ISSET(childout[0], &rfds))
+	  {
+	    /* technically, I should keep reading until everything is
+	     * exhausted... in practice, there isn't enough data to
+	     * exceed MAXLINE.
+	     */
+	    count = read(childout[0], outline, MAXLINE);
+	    outline[count] = '\0';
+
+	    prompt = outline + 2;
+	    outline[1] = '\0';
+	    prompt_type = atoi(outline);
+
+	    switch(prompt_type)
+	      {
+	      case UH_ECHO_ON_PROMPT:
+		message_box = create_query_box(prompt, NULL, 
+					       (GtkSignalFunc)userhelper_write_childin);
+		gtk_widget_show(message_box);
+		break;
+	      case UH_ECHO_OFF_PROMPT:
+		message_box = create_invisible_query_box(prompt, NULL,
+							 (GtkSignalFunc)userhelper_write_childin);
+		gtk_widget_show(message_box);
+		break;
+	      case UH_INFO_MSG:
+		message_box = create_message_box(prompt, NULL);
+		gtk_widget_show(message_box);
+		break;
+	      case UH_ERROR_MSG:
+		message_box = create_error_box(prompt, NULL);
+		gtk_widget_show(message_box);
+		break;
+	      }
+	    
+	  }
+
+	waitpid(pid, &childstatus, WNOHANG);
+
+	if(WIFEXITED(childstatus) != 0)
+	  {
+ 	    retval = 0;
+	  }
+	else
+	  {
+	    tv.tv_sec = 0;
+ 	    tv.tv_usec = 50;	/* long enough? */
+	    retval = select(childout[0] + 1, &rfds, &wfds, NULL, &tv);
+	  }
+      }
+    }
+  else				/* child */
+    {
+      close(childout[0]);
+      close(childin[1]);
+
+      if(childout[1] != STDOUT_FILENO)
+	{
+	  if(dup2(childout[1], STDOUT_FILENO) != STDOUT_FILENO)
+	    {
+	      fprintf(stderr, "dup2() error.\n");
+	      exit(2);
+	    }
+	  close(childout[1]);
+	}
+      if(childin[0] != STDIN_FILENO)
+	{
+	  if(dup2(childin[0], STDIN_FILENO) != STDIN_FILENO)
+	    {
+	      fprintf(stderr, "dup2() error.\n");
+	      exit(2);
+	    }
+	}
+      setbuf(stdout, NULL);
+
+      if(execl(UH_PATH, UH_PATH, UH_PASSWD_OPT, 0) < 0)
+	{
+	  fprintf(stderr, "execl() error, errno=%d\n", errno);
+	}
+
+      _exit(0);
+
+    }
+}
+
+void
+userhelper_run_chfn(char* fullname, char* office, char* officephone,
+		    char* homephone, char* shell)
+{
+
+}
+
+void
+userhelper_parse_exitstatus(int exitstatus)
+{
+
+}
+
+void
+userhelper_parse_childout()
+{
+
+}
+
+int
+userhelper_read_childout()
+{
+
+}
+
+void
+userhelper_write_childin(GtkWidget* widget, GtkWidget* entry)
+{
+  char* input;
+  int len;
+
+  input = gtk_entry_get_text(GTK_ENTRY(entry));
+
+  len = strlen(input);
+  write(childin[1], input, len);
+  write(childin[1], "\n", 1);
+
+}
+
+void
+userhelper_sigchld()
+{
+  /* if the child died, get it's exitstatus and parse it. */
+  /* don't forget to reset the signal handler... */
+}
--- userhelper-wrap.h
+++ userhelper-wrap.h
@@ -1,7 +1,55 @@
-/* -*-Mode: c-*-
- * Author:     Otto Hammersmith
- * File:       .h
- * Created:    <>
- * Time-stamp: <97/02/18 11:10:23 ohammers>
- * Purpose:
+/* -*-Mode: c-*- */
+/* Copyright (C) 1997 Red Hat Software, Inc.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+
+#ifndef __USERHELPER_WRAP_H__
+#define __USERHELPER_WRAP_H__
+
+/* lots 'o includes. */
+#include "userdialogs.h"
+#include <signal.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+
+/* #define UH_PATH "/usr/bin/userhelper" */
+#define UH_PATH "./userhelper"
+#define UH_PASSWD_OPT "-c"
+#define UH_FULLNAME_OPT "-f"
+#define UH_OFFICE_OPT "-o"
+#define UH_OFFICEPHONE_OPT "-p"
+#define UH_HOMEPHONE_OPT "-h"
+
+#define UH_ECHO_ON_PROMPT 1
+#define UH_ECHO_OFF_PROMPT 2
+#define UH_INFO_MSG 3
+#define UH_ERROR_MSG 4
+
+void userhelper_run_passwd();
+void userhelper_run_chfn(char* fullname, char* office, 
+			 char* officephone, char* homephone, 
+			 char* shell);
+void userhelper_parse_exitstatus(int exitstatus);
+void userhelper_parse_childout();
+int userhelper_read_childout();
+/* int userhelper_write_childin(); */
+void userhelper_write_childin(GtkWidget* widget, GtkWidget* entry);
+
+void userhelper_sigchld();	/* sigchld handler */
+
+#endif /* __USERHELPER_WRAP_H__ */
