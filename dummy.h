b49b5a656dbcaf98763ab236c9e24a0608b14c31
ynakai <none@none>
2001-01-06 13:26:39 -0600

    Add dummy.h

---
+++ dummy.h
@@ -0,0 +1,13 @@
+/* Just a dummy file for gettext */
+
+gchar* ud_ok_text = N_("OK");
+gchar* ud_exit_text = N_("Exit");
+gchar* ud_cancel_text = N_("Cancel");
+gchar* ud_fallback_text = N_("Run Unprivileged");
+gchar* label0 = N_("(current) UNIX password: ");
+gchar* label1 = N_("New UNIX password: ");
+gchar* label2 = N_("Retype new UNIX password: ");
+gchar* label3 = N_("BAD PASSWORD: it's WAY too short");
+gchar* label4 = N_("BAD PASSWORD: it is too short");
+gchar* label5 = N_("Password unchanged");
+gchar* label6 = N_("Sorry, passwords do not match");
