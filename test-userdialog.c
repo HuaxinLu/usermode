6e535651380ad11f841d1c1bd7221576443b150e
nalin <none@none>
2003-02-19 00:11:16 -0600

    - add copyright notice - include config.h first - use g_signal_connect instead of gtk_signal_connect - use proper casts for GtkDialog objects

--- test-userdialog.c
+++ test-userdialog.c
@@ -1,10 +1,28 @@
+/*
+ * Copyright (C) 1999,2000,2001,2002,2003 Red Hat, Inc.  All rights reserved.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include "config.h"
 #include <libintl.h>
 #include <locale.h>
 #include <stdio.h>
 #include <glade/glade.h>
 #include <glade/glade-xml.h>
 #include <gtk/gtk.h>
-#include "config.h"
 #include "userdialogs.h"
 
 void
@@ -36,26 +54,25 @@ main(int argc, char *argv[])
 				 "Let's make this a really big message box.",
 				 "Hello");
 
-	gtk_signal_connect_object(GTK_OBJECT(msg), "destroy",
-				  GTK_SIGNAL_FUNC(gtk_main_quit), NULL);
-	gtk_signal_connect(GTK_OBJECT(msg), "destroy",
-			   GTK_SIGNAL_FUNC(hello_world),
-			   (gpointer)"otto");
+	g_signal_connect(G_OBJECT(msg), "destroy",
+			 G_CALLBACK(gtk_main_quit), NULL);
+	g_signal_connect(G_OBJECT(msg), "destroy",
+			 G_CALLBACK(hello_world), "otto");
 
 	msg = create_query_box("Hello world!", "Hi!",
 			       GTK_SIGNAL_FUNC(hello_world2));
-	gtk_dialog_run(msg);
+	gtk_dialog_run(GTK_DIALOG(msg));
 	gtk_widget_destroy(msg);
 
 	msg = create_invisible_query_box("Hello world!", "Hi!",
 					 GTK_SIGNAL_FUNC(hello_world2));
-	gtk_dialog_run(msg);
+	gtk_dialog_run(GTK_DIALOG(msg));
 	gtk_widget_destroy(msg);
 
 	msg = create_error_box("ERROR!\n"
 			       "Let's make this a really big message box.",
 			       NULL);
-	gtk_dialog_run(msg);
+	gtk_dialog_run(GTK_DIALOG(msg));
 	gtk_widget_destroy(msg);
 
 	return 0;
