442ff328d586a27c88666e7af4c66541ff13820c
nalin <none@none>
2003-02-19 00:14:07 -0600

    - update copyright notice

--- usermount.c
+++ usermount.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 1997 Red Hat Software, Inc.
  * Copyright (C) 2001 Red Hat, Inc.
+ * Copyright (C) 2003 Red Hat, Inc.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
