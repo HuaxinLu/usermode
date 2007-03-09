a67cb782074a58ac1a13aa6a37b6295003f3c7c0
mitr <none@none>
2007-03-08 20:29:39 -0600

    2007-03-09  Miloslav Trmac  <mitr@redhat.com>
    	* configure.in: Replace obsolete macros.  Use AC_MSG_CHECKING instead
    	of custom AC_MSG_RESULT usage.  Quote all m4 macro invocations
    	and shell variable expansions where necessary.
    	(AC_INIT): Move the main version number here from usermode.spec.
    	(AM_INIT_AUTOMAKE): Use dist-bzip2, Wall.
    	(PAM_LIBS): Don't pull in -ldl unnecessarily.
    	(ALL_LINGUAS): Move ...
    	* po/LINGUAS: ... here, add missing languages.
    
    	* autogen.sh: Don't use rpm's opt flags by default.  Update for
    	AC_CONFIG_AUX_DIR.  Run autotools in the correct order, and with all
    	warnings enabled.  Don't run configure automatically, only suggest
    	its usage.
    
    	* Makefile.am (VERSION, RELEASE): Remove, rely on configure to set a
    	value.
    	(CVSTAG): Use only $(VERSION).  Use '_' instead of '-' for '.'.
    	(distdir): Remove duplicate definition.
    	(BUILT_SOURCES): New variable.
    	(reaper.c): Remove rule, it breaks compilation with separate build dir.
    	(tag, forcetag, archive): Handle compilation with separate build dir.
    	(archive): Update for new autogen.sh.

--- autogen.sh
+++ autogen.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
-if test -x /bin/rpm ; then
-	if test x${RPM_OPT_FLAGS} = x ; then
-		RPM_OPT_FLAGS=`rpm --eval '%optflags'`
-	fi
-fi
 set -x -e
-CFLAGS="$DEFINES $RPM_OPT_FLAGS -g3 $CFLAGS" ; export CFLAGS
+mkdir -p admin
 glib-gettextize -f
+# intltool bug: it tries to use $aux_dir/po/Makefile.in.in
+ln -s ../po admin/po
 intltoolize --force
-touch config.h.in
+rm admin/po
+
 aclocal
-automake -a
-autoheader
-autoconf
-rm -f config.cache
-./configure --prefix=/usr --sysconfdir=/etc --enable-maintainer-mode $@
-#./configure --prefix=/usr --sysconfdir=/etc $@
+autoconf -Wall
+autoheader -Wall
+automake -Wall --add-missing
+#./configure --prefix=/usr --sysconfdir=/etc --enable-deprecation --with-selinux
