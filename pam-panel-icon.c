2c2d5b72070bb85ac77c7bb2376c5c25b704a818
mitr <none@none>
2007-03-10 20:02:08 -0600

    2007-03-11  Miloslav Trmac  <mitr@redhat.com>
    	* pam-panel-icon.c (child_io_source, child_watch_source): New variables.
    	(running_init_pid): Remove.
    	(handle_drop_response): Split from drop_menu_response_cb.  Don't
    	duplicate dialog creation code.
    	(drop_menu_response_cb): Use the correct prototype.  All users updated.
    	(drop_dialog_response_cb): Use the correct prototype.  Use
    	handle_drop_response ().
    	(drop_menu_items): Make local to handle_popup ().
    	(child_pid): Make local to launch_checker ().
    	(handle_button): Split and integrate into callers.
    	(handle_popup): Use gtk_status_icon_position_menu ().
    	(refresh_tray_icon): Replace ensure_tray_icon (),
    	show_unlocked_icon (), show_locked_icon ().  Use
    	gtk_status_icon_set_visible () instead of switching the pixbuf between
    	NULL and locked_pixbuf.
    	(child_io_func): Remove dead running_init_pid handling.  Don't create
    	copies of the status messages.  Don't poll for the exit status of the
    	checker.
    	(child_watch_func): New function.
    	(launch_checker): Clean up after the previous launch instead of
    	assuming the callers do it.  Watch the checker's exit.
    	(session_save_callback, session_die_callback): Use the correct
    	prototype.
    
    	* pam-panel-icon.c (tray_icon, drop_dialog): Make a void * to avoid
    	an aliasing violation in g_object_add_weak_pointer ().
    	(add_weak_widget_pointer, add_weak_status_icon_pointer): Integrate into
    	the callers.
    
    	* Makefile.am (EXTRA_DIST, INIT_APPS): Remove remaining traces of
    	pam_timestamp_init.
    
    	* pam_timestamp_init.console
    	* pam_timestamp_init.pamd: Remove.
    
    	* pam-panel-icon.c: Remove !HAVE_GTK20 code.
    	* configure.in (HAVE_GTK20): Assume GTK+ 2.10, remove the conditional.
    	* Makefile.am (EXTRA_DIST, EGGFILES, EGGDIR, regenerate-build-sources):
    	Remove eggtrayicon and related files.
    	(pam_panel_icon_SOURCES): Don't use eggtrayicon.
    	* egg-marshal.c
    	* eggmarshalers.list
    	* eggtrayicon.c
    	* eggtrayicon.h
    	* update-from-egg.sh: Remove.
    
    	* configure.in: Get pkg-config data of GTK+.
    	(SELINUX_LIBS): New variable, use separately from LIBS.
    	* Makefile.am (consolehelper_LDADD, consolehelper_gtk_LDADD)
    	(pam_panel_icon_LDADD): Use GTK_LIBS instead of LIBGLADE_LIBS.  Clean
    	up.
    	(pam_panel_icon_SOURCES): Remove unnecessary marshal.c.
    	(userhelper_LDADD): Use SELINUX_LIBS.

--- pam-panel-icon.c
+++ pam-panel-icon.c
@@ -30,10 +30,6 @@
 #include <gtk/gtk.h>
 #include "gsmclient.h"
 
-#ifndef HAVE_GTK210
-#include "eggtrayicon.h"
-#endif
-
 #define PAM_TIMESTAMP_CHECK_PATH "/sbin/pam_timestamp_check"
 
 enum {
@@ -54,30 +50,27 @@ enum {
 
 static int current_status = STATUS_UNKNOWN;
 static GIOChannel *child_io_channel = NULL;
-static pid_t child_pid = -1;
-#ifdef HAVE_GTK210
-static GtkStatusIcon *tray_icon = NULL;
-#else
-static EggTrayIcon *tray_icon = NULL;
-static GtkWidget *image = NULL;
-#endif
-static GtkWidget *drop_dialog = NULL;
+static guint child_io_source; /* = 0; */
+static guint child_watch_source; /* = 0; */
+static void *tray_icon; /* = NULL; actually a GtkStatusIcon * */
+static void *drop_dialog; /* = NULL; actually a GtkWidget * */
 static GtkWidget *drop_menu = NULL;
-static GtkWidget *drop_menu_items = NULL;
 static GdkPixbuf *locked_pixbuf = NULL;
-static pid_t running_init_pid = -1;
 
 static void launch_checker(void);
 
-/* Respond to a selecion in the popup menu. */
-static void
-drop_menu_response_cb( int response_id ) {
-	GError *err;
-	GtkWidget *dialog;
-	int exit_status;
-	char *argv[4];
+ /* Tray icon and authorization dropping */
 
+/* Respond to an user decision about dropping the authorization */
+static void
+handle_drop_response(int response_id)
+{
 	if (response_id == RESPONSE_DROP) {
+		GError *err;
+		GtkWidget *dialog;
+		int exit_status;
+		char *argv[4];
+
 		argv[0] = PAM_TIMESTAMP_CHECK_PATH;
 		argv[1] = "-k";
 		argv[2] = "root";
@@ -85,10 +78,9 @@ drop_menu_response_cb( int response_id ) {
 
 		exit_status = 0;
 		err = NULL;
-		if (!g_spawn_sync("/",
-				  argv, NULL, G_SPAWN_CHILD_INHERITS_STDIN,
-				  NULL, NULL, NULL, NULL,
-				  &exit_status, &err)) {
+		dialog = NULL;
+		if (!g_spawn_sync("/", argv, NULL, G_SPAWN_CHILD_INHERITS_STDIN,
+				  NULL, NULL, NULL, NULL, &exit_status, &err)) {
 			/* There was an error running the command. */
 			dialog = gtk_message_dialog_new(NULL,
 							GTK_DIALOG_DESTROY_WITH_PARENT,
@@ -96,254 +88,147 @@ drop_menu_response_cb( int response_id ) {
 							GTK_BUTTONS_CLOSE,
 							_("Failed to drop administrator privileges: %s"),
 							err->message);
+			g_error_free(err);
+		} else if (WIFEXITED(exit_status)
+			   && WEXITSTATUS(exit_status) != 0) {
+			dialog = gtk_message_dialog_new(NULL,
+							GTK_DIALOG_DESTROY_WITH_PARENT,
+							GTK_MESSAGE_ERROR,
+							GTK_BUTTONS_CLOSE,
+							_("Failed to drop administrator privileges: "
+							  "pam_timestamp_check returned failure code %d"),
+							WEXITSTATUS(exit_status));
+		}
+		if (dialog != NULL) {
 			g_signal_connect(G_OBJECT(dialog), "response",
 					 G_CALLBACK(gtk_widget_destroy),
 					 NULL);
 			gtk_window_set_resizable(GTK_WINDOW(dialog), FALSE);
-			gtk_widget_show(dialog);
-
-			g_error_free(err);
-		} else {
-			if (WIFEXITED(exit_status) &&
-			    (WEXITSTATUS(exit_status) != 0)) {
-				dialog = gtk_message_dialog_new(NULL,
-								GTK_DIALOG_DESTROY_WITH_PARENT,
-								GTK_MESSAGE_ERROR,
-								GTK_BUTTONS_CLOSE,
-								_("Failed to drop administrator privileges: "
-								  "pam_timestamp_check returned failure code %d"),
-								WEXITSTATUS(exit_status));
-				g_signal_connect(G_OBJECT(dialog), "response",
-						 G_CALLBACK(gtk_widget_destroy),
-						 NULL);
-				gtk_window_set_resizable(GTK_WINDOW(dialog),
-							 FALSE);
-				gtk_window_present(GTK_WINDOW(dialog));
-			}
+			gtk_window_present(GTK_WINDOW(dialog));
 		}
 	}
+}
+
+/* Respond to a selectdion in the popup menu. */
+static void
+drop_menu_response_cb(GtkMenuItem *item, gpointer data)
+{
+	handle_drop_response((int)data);
 }
 
 /* Respond to a button press in the drop dialog. */
 static void
-drop_dialog_response_cb(GtkWidget *dialog, int response_id, void *data)
+drop_dialog_response_cb(GtkWidget *dialog, gint response_id, gpointer data)
 {
 	gtk_widget_destroy(dialog);
 
-	drop_menu_response_cb(response_id);
+	handle_drop_response(response_id);
 }
 
 static void
-add_weak_widget_pointer(GObject *object, GtkWidget **weak_pointer)
+handle_activate(GtkStatusIcon *icon, gpointer data)
 {
-	g_object_add_weak_pointer(object, (void**)weak_pointer);
-}
-
-#ifdef HAVE_GTK210
-static void
-add_weak_status_icon_pointer(GObject *object, GtkStatusIcon **weak_pointer)
-#else
-static void
-add_weak_egg_tray_icon_pointer(GObject *object, EggTrayIcon **weak_pointer)
-#endif
-{
-	g_object_add_weak_pointer(object, (void**)weak_pointer);
-}
-
-/* Respond to a button press event on our icon. */
-static gboolean handle_button(guint button, guint activation_time) 
-{
-	/* We only respond to left-click and right click. */
-	if (button != 1 && button != 3) {
-		return FALSE;
-	}
-
 	/* If we're already authenticated, give the user the option of removing
-	 * the timestamp. */
-	if (current_status == STATUS_AUTHENTICATED) {
-		/* Open popup menu when right clicked. */
-		if ( button == 3 ) {
-			if (drop_menu == NULL) {
-				drop_menu = gtk_menu_new();
+	   the timestamp. */
+	if (current_status != STATUS_AUTHENTICATED)
+		return;
 
-				drop_menu_items = gtk_menu_item_new_with_label (_("Keep Authorization"));
-				
-				gtk_menu_shell_append(GTK_MENU_SHELL(drop_menu), drop_menu_items);
+	/* If there's not already a dialog up, create one. */
+	if (drop_dialog == NULL) {
+		drop_dialog = gtk_message_dialog_new(NULL,
+						     GTK_DIALOG_DESTROY_WITH_PARENT,
+						     GTK_MESSAGE_QUESTION,
+						     GTK_BUTTONS_NONE,
+						     _("You're currently authorized to configure system-wide settings (that affect all users) without typing the administrator password again. You can give up this authorization."));
 
-				g_signal_connect_swapped(G_OBJECT(drop_menu_items), "activate",
-							 G_CALLBACK(drop_menu_response_cb),
-							 (gpointer)RESPONSE_DO_NOTHING);
+		g_object_add_weak_pointer(G_OBJECT(drop_dialog), &drop_dialog);
 
-				gtk_widget_show(drop_menu_items);
+		gtk_dialog_add_button(GTK_DIALOG(drop_dialog),
+				      _("Keep Authorization"),
+				      RESPONSE_DO_NOTHING);
+		gtk_dialog_add_button(GTK_DIALOG(drop_dialog),
+				      _("Forget Authorization"), RESPONSE_DROP);
 
-				drop_menu_items = gtk_menu_item_new_with_label (_("Forget Authorization"));
-				
-				gtk_menu_shell_append(GTK_MENU_SHELL(drop_menu), drop_menu_items);
+		g_signal_connect(G_OBJECT(drop_dialog), "response",
+				 G_CALLBACK(drop_dialog_response_cb), NULL);
 
-				g_signal_connect_swapped(G_OBJECT(drop_menu_items), "activate",
-							 G_CALLBACK(drop_menu_response_cb),
-							 (gpointer)RESPONSE_DROP);
-
-				gtk_widget_show(drop_menu_items);
-			}
-			gtk_menu_popup(GTK_MENU(drop_menu), NULL, NULL, NULL, NULL, 
-				       button, activation_time);
-			
-			return TRUE;
-		}
-
-		/* If there's not already a dialog up, create one. */
-		if (drop_dialog == NULL) {
-			drop_dialog = gtk_message_dialog_new(NULL,
-							     GTK_DIALOG_DESTROY_WITH_PARENT,
-							     GTK_MESSAGE_QUESTION,
-							     GTK_BUTTONS_NONE,
-							     _("You're currently authorized to configure system-wide settings (that affect all users) without typing the administrator password again. You can give up this authorization."));
-
-			add_weak_widget_pointer(G_OBJECT(drop_dialog),
-						&drop_dialog);
-
-			gtk_dialog_add_button(GTK_DIALOG(drop_dialog),
-					      _("Keep Authorization"),
-					      RESPONSE_DO_NOTHING);
-
-			gtk_dialog_add_button(GTK_DIALOG(drop_dialog),
-					      _("Forget Authorization"),
-					      RESPONSE_DROP);
-
-			g_signal_connect(G_OBJECT(drop_dialog), "response",
-					 G_CALLBACK(drop_dialog_response_cb),
-					 NULL);
-
-			gtk_window_set_resizable(GTK_WINDOW(drop_dialog),
-						 FALSE);
-		}
-		gtk_window_present(GTK_WINDOW(drop_dialog));
+		gtk_window_set_resizable(GTK_WINDOW(drop_dialog), FALSE);
 	}
-	return TRUE;
+	gtk_window_present(GTK_WINDOW(drop_dialog));
 }
-
-#ifdef HAVE_GTK210
-static gboolean
-handle_activate(GtkStatusIcon *icon, void *data)
-{
-	return handle_button(1, 0);
-}
-
-static gboolean
-handle_popup(GtkStatusIcon *icon, guint button, guint activate_time, void *data)
-{
-	return handle_button(button, activate_time);
-}
-#else
-static gboolean
-icon_clicked_event(GtkWidget *widget, GdkEventButton *event, void *data)
-{
-	return handle_button(event->button, event->time);
-}
-#endif
-
 
 static void
-ensure_tray_icon(void)
+handle_popup(GtkStatusIcon *icon, guint button, guint activate_time,
+	     gpointer data)
 {
+	/* If we're already authenticated, give the user the option of removing
+	   the timestamp. */
+	if (current_status != STATUS_AUTHENTICATED)
+		return;
+	if (drop_menu == NULL) {
+		GtkWidget *item;
+
+		drop_menu = gtk_menu_new();
+
+		item = gtk_menu_item_new_with_label(_("Keep Authorization"));
+		gtk_menu_shell_append(GTK_MENU_SHELL(drop_menu), item);
+		g_signal_connect(G_OBJECT(item), "activate",
+				 G_CALLBACK(drop_menu_response_cb),
+				 (gpointer)RESPONSE_DO_NOTHING);
+		gtk_widget_show(item);
+
+		item = gtk_menu_item_new_with_label(_("Forget Authorization"));
+		gtk_menu_shell_append(GTK_MENU_SHELL(drop_menu), item);
+		g_signal_connect(G_OBJECT(item), "activate",
+				 G_CALLBACK(drop_menu_response_cb),
+				 (gpointer)RESPONSE_DROP);
+		gtk_widget_show(item);
+	}
+	gtk_menu_popup(GTK_MENU(drop_menu), NULL, NULL,
+		       gtk_status_icon_position_menu, icon, button,
+		       activate_time);
+}
+
+static void
+refresh_tray_icon(void)
+{
+	gboolean visible;
+
 	if (tray_icon == NULL) {
-#ifdef HAVE_GTK210
-		tray_icon = gtk_status_icon_new();
-#else
-		tray_icon = egg_tray_icon_new("Authentication Indicator");
-		image = gtk_image_new();
-#endif
+		tray_icon = gtk_status_icon_new_from_pixbuf(locked_pixbuf);
 
 		/* If the system tray goes away, our icon will get destroyed,
 		 * and we don't want to be left with a dangling pointer to it
 		 * if that happens.  */
-#ifdef HAVE_GTK210
-		add_weak_status_icon_pointer(G_OBJECT(tray_icon), &tray_icon);
-		
-		g_signal_connect(G_OBJECT(tray_icon), "activate", G_CALLBACK(handle_activate), NULL);
-		g_signal_connect(G_OBJECT(tray_icon), "popup-menu", G_CALLBACK(handle_popup), NULL);
-#else
-		add_weak_egg_tray_icon_pointer(G_OBJECT(tray_icon), &tray_icon);
-		add_weak_widget_pointer(G_OBJECT(image), &image);
+		g_object_add_weak_pointer(G_OBJECT(tray_icon), &tray_icon);
 
-		/* Add the image to the icon. */
-		gtk_container_add(GTK_CONTAINER(tray_icon), image);
-		gtk_widget_show(image);
-
-		/* Handle clicking events. */
-		gtk_widget_add_events(GTK_WIDGET(tray_icon),
-				      GDK_BUTTON_PRESS_MASK);
-		g_signal_connect(G_OBJECT(tray_icon), "button_press_event",
-				 G_CALLBACK(icon_clicked_event), NULL);
-#endif
+		g_signal_connect(G_OBJECT(tray_icon), "activate",
+				 G_CALLBACK(handle_activate), NULL);
+		g_signal_connect(G_OBJECT(tray_icon), "popup-menu",
+				 G_CALLBACK(handle_popup), NULL);
 	}
-
-#ifdef HAVE_GTK210
-	gtk_status_icon_set_visible(GTK_STATUS_ICON(tray_icon), TRUE);
-#else
-	gtk_widget_show(GTK_WIDGET(tray_icon));
-#endif
+	if (current_status == STATUS_AUTHENTICATED
+	    && (getuid() != 0 || geteuid() != 0 || getgid() != 0
+		|| getegid() != 0))
+		visible = TRUE;
+	else
+		visible = FALSE;
+	gtk_status_icon_set_visible(GTK_STATUS_ICON(tray_icon), visible);
 }
 
-static void
-show_unlocked_icon(void)
-{
-	ensure_tray_icon();
-#ifdef HAVE_GTK210
-	gtk_status_icon_set_from_pixbuf(GTK_STATUS_ICON(tray_icon), NULL);
-#else
-	gtk_image_set_from_pixbuf(GTK_IMAGE(image), NULL);
-	gtk_widget_hide(image);
-#endif
-}
-
-static void
-show_locked_icon(void)
-{
-	if ( getuid() || geteuid() || getgid() || getegid() ) {
-		ensure_tray_icon();
-#ifdef HAVE_GTK210
-		gtk_status_icon_set_from_pixbuf(GTK_STATUS_ICON(tray_icon), locked_pixbuf);
-#else
-		gtk_image_set_from_pixbuf(GTK_IMAGE(image), locked_pixbuf);
-		gtk_widget_show(image);
-#endif
-	}
-}
+ /* Interaction with pam_timestamp_check */
 
 static gboolean
 child_io_func(GIOChannel *source, GIOCondition condition, void *data)
 {
-	int exit_status;
-	int retval;
 	gboolean respawn_child;
 	int output;
 	char *message;
 	int old_status;
-	char buf[10];
 
-	message = NULL;
 	output = 0;
 	respawn_child = FALSE;
 	old_status = current_status;
-
-	/* While we're here, let's check the status of the running_init child */
-	if (running_init_pid != -1) {
-		retval = waitpid(running_init_pid, &exit_status, WNOHANG);
-		if (retval != 0) {
-			if (retval == running_init_pid) {
-				running_init_pid = -1;
-			} else {
-				g_printerr("Confused about waitpid(): %s\n"
-					   "returned %d, child pid was %d\n",
-					   g_strerror(errno), retval,
-					   running_init_pid);
-				running_init_pid = -1;
-			}
-		}
-	}
-
 
 	if (condition & (G_IO_HUP | G_IO_ERR | G_IO_NVAL)) {
 		/* Error conditions mean we need to launch a new child. */
@@ -351,6 +236,7 @@ child_io_func(GIOChannel *source, GIOCondition condition, void *data)
 		current_status = STATUS_UNKNOWN;
 	} else
 	if (condition & G_IO_IN) {
+		char buf[10];
 		GError *err;
 		gsize i;
 		gsize bytes_read;
@@ -370,44 +256,42 @@ child_io_func(GIOChannel *source, GIOCondition condition, void *data)
 		}
 
 		for (i = 0; i < bytes_read; i++) {
-			if (!g_ascii_isdigit(buf[i]) && (buf[i] != '\n')) {
+			if (g_ascii_isdigit(buf[i]))
+				output = atoi(buf + i);
+			else if (buf[i] != '\n')
 				g_printerr("Unknown byte '%d' from "
 					   "pam_timestamp_check.\n",
-					   (int) buf[i]);
-			}
-			if (g_ascii_isdigit(buf[i])) {
-				output = atoi(buf + i);
-			}
+					   (int)buf[i]);
 		}
 	}
 
+	message = NULL;
 	switch (output) {
 	case 0:
 		current_status = STATUS_AUTHENTICATED;
 		break;
 	case 1:
 		current_status = STATUS_UNKNOWN;
-		message = g_strdup("bad args to pam_timestamp_check");
+		message = "bad args to pam_timestamp_check";
 		break;
 	case 2:
-		message = g_strdup(_("pam_timestamp_check is not setuid root"));
+		message = _("pam_timestamp_check is not setuid root");
 		current_status = STATUS_BINARY_NOT_SUID;
 		break;
 	case 3:
-		message =
-		    g_strdup(_("no controlling tty for pam_timestamp_check"));
+		message = _("no controlling tty for pam_timestamp_check");
 		current_status = STATUS_NO_TTY;
 		break;
 	case 4:
-		message = g_strdup(_("user unknown to pam_timestamp_check"));
+		message = _("user unknown to pam_timestamp_check");
 		current_status = STATUS_USER_UNKNOWN;
 		break;
 	case 5:
-		message = g_strdup(_("permissions error in pam_timestamp_check"));
+		message = _("permissions error in pam_timestamp_check");
 		current_status = STATUS_PERMISSIONS_ERROR;
 		break;
 	case 6:
-		message = g_strdup(_("invalid controlling tty in pam_timestamp_check"));
+		message = _("invalid controlling tty in pam_timestamp_check");
 		current_status = STATUS_INVALID_TTY;
 		break;
 
@@ -417,80 +301,69 @@ child_io_func(GIOChannel *source, GIOCondition condition, void *data)
 		break;
 
 	default:
-		message = g_strdup("got unknown code from pam_timestamp_check");
+		message = "got unknown code from pam_timestamp_check";
 		current_status = STATUS_UNKNOWN;
 		break;
 	}
 
 	if (message) {
 		/*  FIXME, dialog? */
-		if (old_status != current_status) {
+		if (old_status != current_status)
 			g_printerr(_("Error: %s\n"), message);
-		}
-		g_free(message);
 	}
 
-	exit_status = 0;
-	retval = waitpid(child_pid, &exit_status, WNOHANG);
-
-	if (retval < 0) {
-		g_printerr("Failed in waitpid(): %s\n", g_strerror(errno));
-	} else
-	if (retval == 0) {
-		/* No child has exited */
-	} else
-	if (retval == child_pid) {
-		/* Child has exited */
-		current_status = STATUS_UNKNOWN;
-		respawn_child = TRUE;
-
-		if (WIFSIGNALED(exit_status)) {
-			g_printerr("pam_timestamp_check died on signal %d\n",
-				   WTERMSIG(exit_status));
-		}
-	} else {
-		g_printerr("Confused about waitpid(): returned %d, child pid "
-			   "was %d\n", retval, child_pid);
-	}
-
-	if (current_status == STATUS_AUTHENTICATED) {
-		show_locked_icon();
-	} else {
-		show_unlocked_icon();
-	}
+	refresh_tray_icon();
 
 	if (respawn_child) {
-		if (child_io_channel != NULL) {
-			g_io_channel_unref(child_io_channel);
-			child_io_channel = NULL;
-			child_pid = -1;
-		}
-
 		/* Respawn the child */
 		launch_checker();
-
 		return FALSE;
-	} else {
+	} else
 		return TRUE;
-	}
+}
+
+static void
+child_watch_func(GPid pid, gint status, gpointer data)
+{
+	if (WIFSIGNALED(status))
+		g_printerr("pam_timestamp_check died on signal %d\n",
+			   WTERMSIG(status));
+
+	current_status = STATUS_UNKNOWN;
+	refresh_tray_icon();
+
+	/* Respawn the child */
+	launch_checker();
 }
 
 /* Launch the child which checks for timestamps. */
 static void
 launch_checker(void)
 {
-	GError *err;
-	char *command[] = {
+	static char *command[] = {
 		PAM_TIMESTAMP_CHECK_PATH,
 		"-d",
 		"root",
 		NULL
 	};
+
+	GPid pid;
+	GError *err;
 	int out_fd;
 
-	/* Don't launch the checker more than once. */
+	/* If we are launching a new checker, destroy all state associated
+	   with the old one. */
+	if (child_io_source != 0) {
+		g_source_remove(child_io_source);
+		child_io_source = 0;
+	}
+	if (child_watch_source != 0) {
+		g_source_remove(child_watch_source);
+		child_watch_source = 0;
+	}
 	if (child_io_channel != NULL) {
-		return;
+		g_io_channel_unref(child_io_channel);
+		child_io_channel = NULL;
 	}
 
 	/* Let the child inherit stdin so that pam_timestamp_check can get at
@@ -498,16 +371,14 @@ launch_checker(void)
 	out_fd = -1;
 	err = NULL;
 	if (!g_spawn_async_with_pipes("/", command, NULL,
-				      G_SPAWN_CHILD_INHERITS_STDIN |
-				      G_SPAWN_DO_NOT_REAP_CHILD,
-				      NULL, NULL, &child_pid, NULL,
-				      &out_fd, NULL, &err)) {
+				      G_SPAWN_CHILD_INHERITS_STDIN
+				      | G_SPAWN_DO_NOT_REAP_CHILD, NULL, NULL,
+				      &pid, NULL, &out_fd, NULL, &err)) {
 		g_printerr(_("Failed to run command \"%s\": %s\n"),
 			   command[0], err->message);
 		g_error_free(err);
 		return;
 	}
-
 	/* We're watching for output from the child. */
 	child_io_channel = g_io_channel_unix_new(out_fd);
 
@@ -519,21 +390,23 @@ launch_checker(void)
 			   err->message);
 		g_error_free(err);
 
-		child_pid = -1;
 		g_io_channel_unref(child_io_channel);
 		child_io_channel = NULL;
 		return;
 	}
 
 	/* Set up a callback for when the child tells us something. */
-	g_io_add_watch(child_io_channel,
-		       G_IO_IN | G_IO_HUP | G_IO_ERR | G_IO_NVAL,
-		       child_io_func, NULL);
+	child_watch_source = g_child_watch_add(pid, child_watch_func, NULL);
+	child_io_source = g_io_add_watch(child_io_channel,
+					 G_IO_IN | G_IO_HUP | G_IO_ERR
+					 | G_IO_NVAL, child_io_func, NULL);
 }
+
+ /* Main program */
 
 /* Save ourselves. */
 static void
-session_save_callback(GsmClient *client, gboolean is_phase2, void *data)
+session_save_callback(GsmClient *client, gboolean is_phase2, gpointer data)
 {
 	const char *argv[4];
 
@@ -552,7 +425,7 @@ session_save_callback(GsmClient *client, gboolean is_phase2, void *data)
 
 /* Handle the "session closing" notification. */
 static void
-session_die_callback(GsmClient *client, void *data)
+session_die_callback(GsmClient *client, gpointer data)
 {
 	gtk_main_quit();
 }
